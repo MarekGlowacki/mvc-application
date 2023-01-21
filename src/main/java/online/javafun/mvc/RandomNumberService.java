4728f41b1deef4657f504e531fde7a2f1e9d0293
User <user@its.me>
2023-01-21 08:27:41 +0100

    getting attributes from controller to html

--- src/main/java/online/javafun/mvc/RandomNumberService.java
+++ src/main/java/online/javafun/mvc/RandomNumberService.java
@@ -1,2 +1,16 @@
-package online.javafun.mvc;public class RandomNumberService {
+package online.javafun.mvc;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomNumberService {
+    private final Random random = new Random();
+
+    int generateRandomInRange(int start, int end) {
+        int range = end - start;
+        int randomNumber = random.nextInt(range);
+        return start + randomNumber;
+    }
 }
